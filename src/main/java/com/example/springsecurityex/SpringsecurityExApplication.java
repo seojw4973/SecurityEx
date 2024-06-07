4af4109d3290dd6e8343480a08ff5dcdbcc8b206
seojw4973 <sgw0816@naver.com>
2024-06-07 21:47:11 +0900

    테이블 생성 및 jpa를 통한 로그인 구현

--- src/main/java/com/example/springsecurityex/SpringsecurityExApplication.java
+++ src/main/java/com/example/springsecurityex/SpringsecurityExApplication.java
@@ -2,8 +2,15 @@ package com.example.springsecurityex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+// 메인 패키지 외부에 엔티티나 레포지터리 작성시 다음과 같이 어노테이션을 추가해주어야함
+//@EnableJpaRepositories("com.example.springsecurityex.repository")
+//@EntityScan("com.example.springsecurityex.model")
+// @EnableWebSecurity      // spring boot 없이 spring 개발시 언급해주어야 시큐리티의 모든 기능이 활성화됨
 @SpringBootApplication
 public class SpringsecurityExApplication {
 
--- src/main/java/com/example/springsecurityex/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecurityex/config/ProjectSecurityConfig.java
@@ -6,7 +6,13 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -14,14 +20,23 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> {
-            requests.anyRequest().permitAll();
+//            requests.anyRequest().permitAll();
 //            requests.anyRequest().denyAll();
-//            requests.requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
-//                    .requestMatchers("/contact", "/notices").permitAll();
+            requests.requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
+                    .requestMatchers("/contact", "/notices").permitAll();
         });
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
     }
+
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {return NoOpPasswordEncoder.getInstance();}
+
 
 }
