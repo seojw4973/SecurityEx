4820cfaf0de0943c2a5199dce397f0a288777051
seojw4973 <sgw0816@naver.com>
2024-06-08 22:03:03 +0900

    BCryptPasswordEncoder를 이용한 비밀번호 해싱

--- src/main/java/com/example/springsecurityex/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/springsecurityex/config/ProjectSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -26,55 +27,16 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests((requests) -> {
-//            requests.anyRequest().permitAll();
-//            requests.anyRequest().denyAll();
             requests.requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
                     .requestMatchers("/contact", "/notices", "/register").permitAll();
         });
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
-        return (SecurityFilterChain)http.build();
+        return http.build();
     }
 
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//
-        // 1. withDefaultPasswordEncoder() 메소드를 사용해서 인메모리에 유저 자격증명 저장
-        //        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("admin")
-//                .authorities("admin")
-//                .build();
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("user")
-//                .authorities("read")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
-
-        // 2. NoOpPasswordEncoder() 메소드를 통한 인메모리 유저 자격증명 저장
-        // 1과 2는 차이가 딱히 없음
-//        UserDetails admin = User.withUsername("admin")
-//                .password("12345")
-//                .authorities("admin")
-//                .build();
-//        UserDetails user = User.withUsername("user")
-//                .password("12345")
-//                .authorities("read")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
-
-
-    /*@Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }*/
-
     @Bean
-    public PasswordEncoder passwordEncoder() {return NoOpPasswordEncoder.getInstance();}
+    public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
 
 
 }
--- src/main/java/com/example/springsecurityex/controller/LoginController.java
+++ src/main/java/com/example/springsecurityex/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.example.springsecurityex.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
     CustomerRepository customerRepository;
 
     @PostMapping("/register")
@@ -21,6 +25,8 @@ public class LoginController {
         Customer savedCustomer = null;
         ResponseEntity response = null;
         try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
             savedCustomer = customerRepository.save(customer);
             if (savedCustomer.getId() > 0) {
                 response = ResponseEntity
