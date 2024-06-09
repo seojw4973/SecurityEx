5f3b148bb594dc291c62b163c2e0efd6c08274db
seojw4973 <sgw0816@naver.com>
2024-06-09 17:06:03 +0900

    메소드 수준에서의 보안 PreAuthorize/PostAuthorize
    PreFilter/PostFilter

--- src/main/java/com/example/springsecurityex/SpringsecurityExApplication.java
+++ src/main/java/com/example/springsecurityex/SpringsecurityExApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 // 메인 패키지 외부에 엔티티나 레포지터리 작성시 다음과 같이 어노테이션을 추가해주어야함
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 //@EntityScan("com.example.springsecurityex.model")
 // @EnableWebSecurity      // spring boot 없이 spring 개발시 언급해주어야 시큐리티의 모든 기능이 활성화됨
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SpringsecurityExApplication {
 
     public static void main(String[] args) {
